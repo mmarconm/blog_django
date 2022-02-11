72daf58b76c3f58509f1a8f7b2c0a5d0d3ba7e7b
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-02-10 20:40:15 -0400

    added article app to blog urls

---
+++ articles/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import (
+    articles_list,
+)
+
+urlpatterns = [
+    path('', articles_list),
+]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def articles_list(request):
+    return HttpResponse('<h1>Articles List</h1>')
