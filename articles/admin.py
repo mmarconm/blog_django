7605cb94c6d3c8c11f3a757b21d5a7acf9a71445
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-02-11 20:43:11 -0400

    added a article model

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from articles.models import Article
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published_at')
+    list_filter = ('author',)
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_at'
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=240)
+    description = models.TextField()
+    slug = models.SlugField(max_length=100, unique=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
